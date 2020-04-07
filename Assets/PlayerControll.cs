331df79b438df3cd78e5fe1e7a96b331d73765a9
Claire Wang <hybridm02@gmail.com>
2020-04-08 00:10:35 +0800

    camera + raycast + level model

---
+++ Assets/PlayerControll.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControll : MonoBehaviour
+{
+    // public Rigidbody thisRigidbody;
+    public CharacterController thisCharacterController;
+    public float forwardBackward;
+    public float rightLeft;
+    public float mouseX, mouseY;
+    public float moveSpeed = 2f;
+    public Vector3 inputVector;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        thisCharacterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        //  GET MOUSELOOK
+        mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(0, mouseX, 0);
+
+        mouseY = Input.GetAxis("Mouse Y");
+        //Camera.main.transform.Rotate(-mouseY, 0, 0);
+
+        xRotation -= mouseY; 
+        xRotation = Mathf.Clamp(xRotation, -30f, 75f);
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0, 0); 
+
+
+        forwardBackward = Input.GetAxis("Vertical");
+        rightLeft = Input.GetAxis("Horizontal");
+
+        inputVector = transform.forward * forwardBackward;
+        inputVector += transform.right * rightLeft;
+        thisCharacterController.Move(inputVector * moveSpeed + (Physics.gravity * .69f));
+    }
+   
+}
---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+    public float maxRaycastDist = 1000f;
+    //public Material highlightMaterial;
+    //public Material defaultMaterial;
+
+    private Transform _selection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //change material if deselect 
+        if (_selection != null)
+        {
+        //    if (_selection.CompareTag("Selectable") || _selection.CompareTag("Interactable"))
+        //    {
+        //        var selectionRenderer = _selection.GetComponent<MeshRenderer>();
+        //        narrativeText.text = "";
+        //        //selectionRenderer.material = defaultMaterial;
+        //        //selectionRenderer.material = _selection.GetComponent<PlayerInteract>().defaultMaterial;
+        //        _selection = null;
+        //    } else if (_selection.CompareTag("Posters"))
+        //    {
+        //        var selectionRenderer = _selection.GetComponent<MeshRenderer>();
+        //        narrativeText.text = "";
+        //        //turn off specular highlights for posters
+        //        selectionRenderer.material.EnableKeyword("_SPECULARHIGHLIGHTS_OFF");
+        //        selectionRenderer.material.SetFloat("_SpecularHighlights", 0f);
+        //        _selection = null;
+        //    }
+        }
+
+
+        //select 
+        Ray camRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        Debug.DrawRay(camRay.origin, camRay.direction * maxRaycastDist, Color.green);
+
+        RaycastHit hitObject;
+        if (Physics.Raycast(camRay, out hitObject, maxRaycastDist))
+        {
+            var selection = hitObject.transform;
+                if (Input.GetMouseButton(0))
+                {
+                    //interact 
+                }
+                _selection = selection;
+
+        }
+    }
+}
