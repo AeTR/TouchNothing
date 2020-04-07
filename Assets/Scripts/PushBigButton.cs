5e7a4c4577528ef9aabd3cfd7982733bcd5d0dd5
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2020-04-07 09:33:15 -0700

    Big Red Button Animation

---
+++ Assets/Scripts/PushBigButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBigButton : MonoBehaviour
+{
+    public Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myAnimator.SetBool("Pressing", Input.GetKey(KeyCode.Space));
+    }
+}
