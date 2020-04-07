4c6f1a09e85229d9e5e37065bc8d5be8cdb17915
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2020-04-07 08:52:04 -0700

    All practice animations

---
+++ Assets/Scripts/AnimationChooser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChooser : MonoBehaviour
+{
+    public Animator thisAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            thisAnimator.SetTrigger("Kyle");
+        }
+    }
+}
