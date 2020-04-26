fe0cd93b383c702ccd7b902bbbf5399f818ab430
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2020-04-26 14:20:47 -0700

    Implemented Switch Puzzle

---
+++ Assets/Scripts/SingleSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleSwitch : MechanismScript
+{
+    public bool up;
+
+    public SwitchPuzzle mySwitchPuzzle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public new void Interact()
+    {
+        myAnimator.SetTrigger(interactTrigger);
+        if (up)
+        {
+            Debug.Log("Currently up");
+            up = false;
+        }
+        else
+        {
+            Debug.Log("Currently down");
+            up = true;
+        }
+        mySwitchPuzzle.CheckAllSwitches();
+    }
+}
---
+++ Assets/Scripts/SwitchPuzzle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPuzzle : MonoBehaviour
+{
+    public SingleSwitch[] allSwitches;
+
+    public bool[] switchSolution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchSolution = new[] {true, true, false, true, false, false, true, true, false, true};
+    }
+
+    public void CheckAllSwitches()
+    {
+        Debug.Log("Now checking");
+        bool correct = true;
+        for (int i = 0; i < 10; i++)
+        {
+            if (allSwitches[i].up != switchSolution[i])
+            {
+                correct = false;
+            }
+        }
+
+        if (correct)
+        {
+            Debug.Log("You did it!");
+            //do a thing
+        }
+        else
+        {
+            Debug.Log("You failed");
+        }
+    }
+}
