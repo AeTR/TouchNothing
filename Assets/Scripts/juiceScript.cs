95c8e79f15efab35b6946aa068b834ed0afceaf5
Euris Adino <ea1969@nyu.edu>
2020-04-28 11:30:49 -0400

    Class stuff out of recklessness
    Oops, sorry! Wont happen again

---
+++ Assets/Scripts/juiceScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class juiceScript : MonoBehaviour
+{
+    public Transform thisDestination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(coundownCoffee());
+    }
+
+    IEnumerator coundownCoffee()
+    {
+        Debug.Log("WOAH");
+
+        yield return null;
+
+        Debug.Log("Waited for 1 frame");
+        
+        yield return new WaitForSeconds(1.0f);
+        
+        Debug.Log("3...");
+        
+        yield return new WaitForSeconds(1.0f);
+        
+        Debug.Log("2...");
+        
+        yield return new WaitForSeconds(1.0f);
+        
+        Debug.Log("1...");
+        
+        yield return new WaitForSeconds(1.0f);
+        
+        Debug.Log("Go!");
+        
+        //yield return new WaitForSeconds(1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += thisDestination.position - transform.position) *.1f;
+    }
+}
---
+++ Assets/curveScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class curveScript : MonoBehaviour
+{
+    public AnimationCurve animCurve;
+    public Transform curveBoi;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space));
+        {
+            StartCoroutine(SmoothOperator());
+        }
+    }
+
+    private IEnumerator SmoothOperator()
+    {
+        Vector3 start = this.transform.position;
+
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+
+            transform.position = Vector3.LerpUnclamped(start, curveBoi.position, animCurve.Evaluate(t));
+
+            yield return null;
+        }
+    }
+}
