5c5d8f5ebc3410f30b72062451842d9bb81c19f5
Alex Theologides Rodriguez <atheologidesrodriguez@gmail.com>
2020-05-03 01:34:02 -0700

    Intro Scene and monologue manager

---
+++ Assets/Scripts/MonoManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MonoManager : MonoBehaviour
+{
+    public string[] textLines;
+    public AudioSource mySource;
+    public AudioClip[] monologueClips;
+    public Text monologueText, hintText;
+    public int currentNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentNum = 0;
+        hintText.text = "Click to continue";
+        LoadMonologue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (currentNum < 12)
+            {
+                currentNum++;
+                LoadMonologue();
+            }
+            else
+            {
+                SceneManager.LoadScene("PlayableTestScene");
+            }
+        }
+    }
+
+    public void LoadMonologue()
+    {
+        if (currentNum >= 12)
+        {
+            hintText.text = "Click to begin!";
+        }
+        mySource.PlayOneShot(monologueClips[currentNum]);
+        monologueText.text = textLines[currentNum];
+    }
+    
+}
